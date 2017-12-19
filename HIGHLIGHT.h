02292749976148e63a3d1749852ffaf723c88895
Niraj Narkhede <narkhede400@gmail.com>
2017-12-19 09:30:53 +0530

    Add files via upload

---
+++ HIGHLIGHT.h
@@ -0,0 +1,24 @@
+
+#include <windows.h>
+#include <stdio.h>
+#include <conio.h>
+
+void col_back(int ForgC, int BackC)
+{
+     WORD wColor = ((BackC & 0x0F) << 4) + (ForgC & 0x0F);;
+     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), wColor);
+     return;
+}
+
+/////////////////////////////////////////////////////////////////
+void gotoxy(int x,int y)
+{
+	COORD c;
+	c.X = x;
+	c.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),c);
+}
+	
+/////////////////////////////////////////////////////////////////////
+      
+	  
